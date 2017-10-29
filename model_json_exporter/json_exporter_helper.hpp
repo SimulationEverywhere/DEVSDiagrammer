3b93f02fc2590c58717294352eaf0be919181b6a
Laouen <laouen.belloli@gmail.com>
2017-10-29 16:31:39 -0300

    implemented the model json exporter

--- model_json_exporter/json_exporter_helper.hpp
+++ model_json_exporter/json_exporter_helper.hpp
@@ -6,15 +6,17 @@
 #include <boost/type_index.hpp>
 #include <boost/property_tree/ptree.hpp>
 #include <cadmium/modeling/message_bag.hpp>
+#include <cadmium/modeling/ports.hpp>
 
 using boost::property_tree::ptree;
+using cadmium::make_message_bags;
 
 /*******************************************************/
 /**************** IC export to json ********************/
 /*******************************************************/
 
 template<typename TIME, typename ICs, std::size_t S>
-struct add_in_tree_IC_impl {
+struct IC_to_json_impl {
     using current_IC=typename std::tuple_element<S - 1, ICs>::type;
     using from_model=typename current_IC::template from_model<TIME>;
     using from_port=typename current_IC::from_model_output_port;
@@ -30,22 +32,178 @@ struct add_in_tree_IC_impl {
         current_json_ic.put("to_port", boost::typeindex::type_id<to_port>().pretty_name());
         json_IC_list.push_back(std::make_pair("", current_json_ic));
 
-        add_in_tree_IC_impl<TIME, ICs, S - 1>::print(json_IC_list);
+        IC_to_json_impl<TIME, ICs, S - 1>::print(json_IC_list);
     }
 };
 
 template<typename TIME, typename ICs>
-struct add_in_tree_IC_impl<TIME, ICs, 0> {
-    static void print(ptree& json_model) { /* do nothing, this is the base case */ }
+struct IC_to_json_impl<TIME, ICs, 0> {
+    static void print(ptree& json_IC_list) { /* do nothing, this is the base case */ }
 };
 
 template<typename TIME, typename ICs>
-void add_in_tree_IC(ptree& json_model) {
+void IC_to_json(ptree &json_model) {
 
     ptree json_IC_list;
-    add_in_tree_IC_impl<TIME, ICs, std::tuple_size<ICs>::value>::print(json_IC_list);
+    IC_to_json_impl<TIME, ICs, std::tuple_size<ICs>::value>::print(json_IC_list);
 
     json_model.add_child("ic", json_IC_list);
+};
+
+/*******************************************************/
+/**************** EIC export to json ********************/
+/*******************************************************/
+
+template<typename TIME, typename EICs, std::size_t S>
+struct EIC_to_json_impl {
+    using current_EIC=typename std::tuple_element<S-1, EICs>::type;
+    using from_port=typename current_EIC::external_input_port;
+    using to_model=typename current_EIC::template submodel<TIME>;
+    using to_port=typename current_EIC::submodel_input_port;
+
+    static void print(ptree& json_EIC_list) {
+
+        ptree current_json_eic;
+        current_json_eic.put("from_port", boost::typeindex::type_id<from_port>().pretty_name());
+        current_json_eic.put("to_model", boost::typeindex::type_id<to_model>().pretty_name());
+        current_json_eic.put("to_port", boost::typeindex::type_id<to_port>().pretty_name());
+        json_EIC_list.push_back(std::make_pair("", current_json_eic));
+
+        EIC_to_json_impl<TIME, EICs, S - 1>::print(json_EIC_list);
+    }
+};
+
+template<typename TIME, typename EICs>
+struct EIC_to_json_impl<TIME, EICs, 0> {
+    static void print(ptree& json_EIC_list) { /* do nothing, this is the base case */ }
+};
+
+template<typename TIME, typename EICs>
+void EIC_to_json(ptree &json_model) {
+
+    ptree json_EIC_list;
+    EIC_to_json_impl<TIME, EICs, std::tuple_size<EICs>::value>::print(json_EIC_list);
+
+    json_model.add_child("eic", json_EIC_list);
+};
+
+/*******************************************************/
+/**************** EOC export to json ********************/
+/*******************************************************/
+
+template<typename TIME, typename EOCs, std::size_t S>
+struct EOC_to_json_impl {
+    using current_EOC=typename std::tuple_element<S-1, EOCs>::type;
+    using to_port=typename current_EOC::external_output_port;
+    using from_model = typename current_EOC::template submodel<TIME>;
+    using from_port=typename current_EOC::submodel_output_port;
+
+    static void print(ptree& json_EOC_list) {
+
+        ptree current_json_eoc;
+        current_json_eoc.put("to_port", boost::typeindex::type_id<to_port>().pretty_name());
+        current_json_eoc.put("from_model", boost::typeindex::type_id<from_model>().pretty_name());
+        current_json_eoc.put("from_port", boost::typeindex::type_id<from_port>().pretty_name());
+
+        json_EOC_list.push_back(std::make_pair("", current_json_eoc));
+
+        EOC_to_json_impl<TIME, EOCs, S - 1>::print(json_EOC_list);
+    }
+};
+
+template<typename TIME, typename EOCs>
+struct EOC_to_json_impl<TIME, EOCs, 0> {
+    static void print(ptree& json_EOC_list) { /* do nothing, this is the base case */ }
+};
+
+template<typename TIME, typename EOCs>
+void EOC_to_json(ptree &json_model) {
+
+    ptree json_EOC_list;
+    EOC_to_json_impl<TIME, EOCs, std::tuple_size<EOCs>::value>::print(json_EOC_list);
+
+    json_model.add_child("eoc", json_EOC_list);
+};
+
+/*******************************************************/
+/*************** ports export to json ******************/
+/*******************************************************/
+
+template<typename TIME, typename PORTS, std::size_t S>
+struct ports_to_json_impl {
+    using current_port=typename std::tuple_element<S-1, PORTS>::type;
+    using message_type=typename current_port::message_type;
+
+    static void print(ptree& json_ports_list) {
+
+        ptree current_json_port;
+        current_json_port.put("name", boost::typeindex::type_id<current_port>().pretty_name());
+        current_json_port.put("message_type", boost::typeindex::type_id<message_type>().pretty_name());
+
+        if (current_port::kind == cadmium::port_kind::out) {
+            current_json_port.put("port_kind", "out");
+        } else {
+            current_json_port.put("port_kind", "in");
+        }
+
+        json_ports_list.push_back(std::make_pair("", current_json_port));
+
+        ports_to_json_impl<TIME, PORTS, S - 1>::print(json_ports_list);
+    }
+};
+
+template<typename TIME, typename PORTS>
+struct ports_to_json_impl<TIME, PORTS, 0> {
+    static void print(ptree& json_ports_list) { /* do nothing, this is the base case */ }
+};
+
+template<typename TIME, typename PORTS>
+void ports_to_json(ptree &json_model, cadmium::port_kind outin) {
+
+    ptree json_ports_list;
+    ports_to_json_impl<TIME, PORTS, std::tuple_size<PORTS>::value>::print(json_ports_list);
+
+    if (outin == cadmium::port_kind::out) {
+        json_model.add_child("out", json_ports_list);
+    } else {
+        json_model.add_child("in", json_ports_list);
+    }
+};
+
+/*******************************************************/
+/************* submodels export to json ****************/
+/*******************************************************/
+
+template<typename TIME,  template<typename> class SUBMODELS, template<template<typename MT> typename M> typename COUPLED_JSON_EXPORTER, template<template<typename MT> typename M> typename ATOMIC_JSON_EXPORTER, std::size_t S>
+struct submodels_to_json_impl {
+
+    template <typename T>
+    using current_submodel=typename std::tuple_element<S - 1, SUBMODELS<T>>::type;
+    using submodel_json_exporter=typename std::conditional<cadmium::concept::is_atomic<current_submodel>::value(), ATOMIC_JSON_EXPORTER<current_submodel>, COUPLED_JSON_EXPORTER<current_submodel>>::type;
+
+    static void print(ptree& json_submodels_list) {
+
+        ptree current_json_submodel;
+        submodel_json_exporter exporter;
+        exporter.export_to_json(current_json_submodel);
+
+        json_submodels_list.push_back(std::make_pair("", current_json_submodel));
+
+        submodels_to_json_impl<TIME, SUBMODELS, COUPLED_JSON_EXPORTER, ATOMIC_JSON_EXPORTER, S - 1>::print(json_submodels_list);
+    }
+};
+
+template<typename TIME,  template<typename> typename SUBMODELS, template<template<typename MT> typename M> typename COUPLED_JSON_EXPORTER, template<template<typename MT> typename M> typename ATOMIC_JSON_EXPORTER>
+struct submodels_to_json_impl<TIME, SUBMODELS, COUPLED_JSON_EXPORTER, ATOMIC_JSON_EXPORTER, 0> {
+    static void print(ptree& json_submodels_list) { /* do nothing, this is the base case */ }
+};
+
+template<typename TIME,  template<typename> typename SUBMODELS, template<template<typename MT> typename M> typename COUPLED_JSON_EXPORTER, template<template<typename MT> typename M> typename ATOMIC_JSON_EXPORTER>
+void submodels_to_json(ptree &json_model) {
+
+    ptree json_submodels_list;
+    submodels_to_json_impl<TIME, SUBMODELS, COUPLED_JSON_EXPORTER, ATOMIC_JSON_EXPORTER, std::tuple_size<SUBMODELS<TIME>>::value>::print(json_submodels_list);
+    json_model.add_child("models", json_submodels_list);
 };
 
 #endif //PMGBP_PDEVS_TUPLE_PRINTER_HPP_HPP
--- model_json_exporter/model_json_exporter.hpp
+++ model_json_exporter/model_json_exporter.hpp
@@ -1,5 +1,6 @@
 #ifndef PMGBP_PDEVS_MODEL_JSON_EXPORTER
 #define PMGBP_PDEVS_MODEL_JSON_EXPORTER
+
 #include <iostream>
 #include <string>
 
@@ -17,34 +18,102 @@ using cadmium::make_message_bags;
 using boost::property_tree::ptree;
 using boost::property_tree::write_json;
 
-template<template<typename T> class MODEL, typename TIME>
+
+template<typename TIME, template<typename T> class MODEL>
+class Atomic_cadmiun_to_json {
+    using output_ports=typename MODEL<TIME>::output_ports;
+    using input_ports=typename MODEL<TIME>::input_ports;
+
+public:
+    Atomic_cadmiun_to_json() = default;
+
+    void export_to_json(ptree& json_model) {
+
+        json_model.put("id", boost::typeindex::type_id<MODEL<TIME>>().pretty_name());
+        json_model.put("type", "atomic");
+
+        ptree json_ports_model;
+        ports_to_json<TIME, output_ports>(json_ports_model, cadmium::port_kind::out);
+        ports_to_json<TIME, input_ports>(json_ports_model, cadmium::port_kind::in);
+
+        json_model.add_child("ports", json_ports_model);
+    }
+
+    void print_to_json(const char* json_file_path) {
+        ptree json_model;
+        this->export_to_json(json_model);
+
+        std::ostringstream buf;
+        ofstream json_file;
+        json_file.open (json_file_path);
+
+        write_json (buf, json_model, true);
+        json_file << buf.str();
+
+        json_file.close();
+    }
+};
+
+template<typename TIME, template<typename T> class MODEL>
 class Cadmium_to_JSON {
 
     template<typename P>
     using submodels_type=typename MODEL<TIME>::template models<P>;
-    using in_bags_type=typename make_message_bags<typename MODEL<TIME>::input_ports>::type;
-    using out_bags_type=typename make_message_bags<typename MODEL<TIME>::output_ports>::type;
+    using output_ports=typename MODEL<TIME>::output_ports;
+    using input_ports=typename MODEL<TIME>::input_ports;
     using eic=typename MODEL<TIME>::external_input_couplings;
     using eoc=typename MODEL<TIME>::external_output_couplings;
     using ic=typename MODEL<TIME>::internal_couplings;
 
-private:
+    template<template<typename> class M>
+    using coupled_submodel_exporter=Cadmium_to_JSON<TIME, M>;
 
+    template<template<typename> class M>
+    using atomic_submodel_exporter=Atomic_cadmiun_to_json<TIME, M>;
 
 public:
-    Cadmium_to_JSON(const char* json_file) {
+    Cadmium_to_JSON() = default;
 
+    void export_to_json(ptree& json_model) {
+
+        json_model.put("id", boost::typeindex::type_id<MODEL<TIME>>().pretty_name());
+        json_model.put("type", "coupled");
+
+        IC_to_json<TIME, ic>(json_model);
+        EIC_to_json<TIME, eic>(json_model);
+        EOC_to_json<TIME, eoc>(json_model);
+
+        ptree json_ports_model;
+        ports_to_json<TIME, output_ports>(json_ports_model, cadmium::port_kind::out);
+        ports_to_json<TIME, input_ports>(json_ports_model, cadmium::port_kind::in);
+
+        json_model.add_child("ports", json_ports_model);
+
+        submodels_to_json<TIME, submodels_type, coupled_submodel_exporter, atomic_submodel_exporter>(json_model);
     }
 
-    void export_to_json() {
-
+    void print_to_json(const std::string& json_file_path) {
         ptree json_model;
-        add_in_tree_IC<TIME, ic>(json_model);
-        std::ostringstream buf;
-        write_json (buf, json_model, false);
-        std::cout << buf.str();
+        this->export_to_json(json_model);
 
+        std::ostringstream buf;
+        ofstream json_file;
+        json_file.open (json_file_path);
+
+        write_json (buf, json_model, true);
+        json_file << buf.str();
+
+        json_file.close();
     }
 };
+
+template<typename TIME, template<typename T> class MODEL>
+void export_model_to_json(const char* json_file_path) {
+    using json_exporter=typename std::conditional<cadmium::concept::is_atomic<MODEL>::value(), Atomic_cadmiun_to_json<TIME, MODEL>, Cadmium_to_JSON<TIME, MODEL>>::type;
+
+    json_exporter exporter;
+    exporter.print_to_json(json_file_path);
+};
+
 
 #endif //PMGBP_PDEVS_MODEL_JSON_EXPORTER
